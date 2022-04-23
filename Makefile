83b704e424cdbd7fee9f45b2a32fb033f94d156d
Bastien <bastien.bordereau@gmail.com>
2022-04-23 22:07:07 +0200

    add new functions

--- Makefile
+++ Makefile
@@ -14,13 +14,15 @@ INCLUDES = includes/
 
 FILES_STR = ft_strlen.c ft_strlcpy.c ft_strlcat.c ft_toupper.c ft_tolower.c ft_strchr.c ft_strrchr.c ft_strncmp.c \
 			ft_strnstr.c ft_atoi.c ft_strdup.c ft_substr.c ft_strjoin.c ft_strtrim.c ft_itoa.c ft_strmapi.c \
-			ft_striteri.c ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c ft_split.c
+			ft_striteri.c ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c ft_split.c ft_strcpy.c \
+			ft_strcmp.c ft_strstr.c ft_strreplace.c ft_strcat.c ft_atol.c ft_strcapitalize.c \
+			ft_get_index.c
 
-FILES_IS = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c
+FILES_IS = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c ft_isspace.c
 
 FILES_LST = ft_lstnew.c ft_lstadd_front.c ft_lstsize.c ft_lstadd_back.c ft_lstdelone.c ft_lstclear.c ft_lstlast.c ft_lstiter.c ft_lstmap.c
 
-FILES_MEM = ft_memset.c ft_bzero.c ft_memcpy.c ft_memmove.c ft_memchr.c ft_memcmp.c ft_calloc.c
+FILES_MEM = ft_memset.c ft_bzero.c ft_memcpy.c ft_memmove.c ft_memchr.c ft_memcmp.c ft_calloc.c ft_free_tab.c ft_realloc.c
 
 FILES_FT_PRINTF = ft_printf.c ft_printf_utils.c
 
--- includes/libft.h
+++ includes/libft.h
@@ -52,14 +52,19 @@ int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_atoi(const char *str);
+int		ft_atol(const char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_lstsize(t_list *lst);
 int		ft_printf(const char *str, ...);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_isspace(int c);
 
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+
+ssize_t	ft_get_index(char *str, char c);
 
 void	*ft_memset(void *str, int c, size_t len);
 void	*ft_calloc(size_t count, size_t size);
@@ -82,17 +87,24 @@ void	ft_lstadd_back(t_list **alst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
+void	*ft_free_tab(void **tab);
+void	*ft_realloc(void *ptr, size_t size);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strchr( const char *str, int c);
 char	*ft_strrchr( const char *str, int c);
 char	*ft_strnstr(const char *str, const char *to_find, size_t len);
+char	*ft_strstr(char *str, char *to_find);
 char	*ft_strdup(const char *source);
+char	*ft_strcat(char *dest, char *src);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_itoa(int n);
 char	*get_next_line(int fd);
+char	*ft_strreplace(char *old, char *new);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	**ft_split(char const *s, char c);
+char	*ft_strcpy(char *dest, char *src);
+char	*ft_strcapitalize(char *str);
 
 #endif
